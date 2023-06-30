b6bbce7d0f2353e5b6965a03a4a31ca32bf5ad6d
Moises Arcos <acm.developer.19@gmail.com>
2023-06-30 05:31:39 -0600

    DWC-690-B: Confirmar cuenta

---
+++ views/auth/confirmar.php
@@ -0,0 +1,13 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Registrate en DevWebcamp</p>
+    <?php
+        require_once __DIR__ . '/../templates/alertas.php';
+    ?>
+
+    <?php if (isset($alertas['exito'])) { ?>
+        <div class="acciones--centrar">
+            <a href="/login" class="acciones__enlace">Iniciar sesión</a>
+        </div>
+    <?php } ?>
+</main>

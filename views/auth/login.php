bbce6d566d255c4c8c30ec7def370ce8cc5c2dec
Moises Arcos <acm.developer.19@gmail.com>
2023-06-30 06:09:51 -0600

    DWC-692: Autenticando a los usuarios

--- views/auth/login.php
+++ views/auth/login.php
@@ -3,7 +3,9 @@
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Iniciar sesión en DevWebcamp</p>
 
-    <form action="" class="formulario">
+    <?php require_once __DIR__ . '/../templates/alertas.php'; ?>
+
+    <form method="POST" action="/login" class="formulario">
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
             <input 

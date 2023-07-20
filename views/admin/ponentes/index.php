c79fa0a31c613e42d4c554c4537cda307581487f
Moises Arcos <acm.developer.19@gmail.com>
2023-07-19 20:03:28 -0600

    DWC-711: CSS al listado de Ponentes

--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -11,7 +11,7 @@
     <?php if(!empty($ponentes)){ ?>
 
         <table class="table">
-            <thead>
+            <thead class="table__thead">
                 <tr>
                     <th scope="col" class="table__th">Nombre</th>
                     <th scope="col" class="table__th">Ubicación</th>
@@ -28,12 +28,12 @@
                             <?php echo $ponente->ciudad . ", " . $ponente->pais; ?>
                         </td>
                         <td class="table__td--acciones">
-                            <a href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>">
+                            <a class="table__accion table__accion--editar" href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>">
                                 <i class="fa-solid fa-user-pen"></i>
                                 Editar
                             </a>
                             <form action="" class="table__formulario">
-                                <button type="submit">
+                                <button class="table__accion table__accion--eliminar" type="submit">
                                     <i class="fa-solid fa-circle-xmark"></i>
                                     Eliminar
                                 </button>

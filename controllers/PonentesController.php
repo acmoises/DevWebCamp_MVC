ba4cba11a4773d9bf5e7e6684f2e0fd2ce2ad167
Moises Arcos <acm.developer.19@gmail.com>
2023-08-03 06:10:32 -0600

    DWC-725: Paginar los ponentes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -22,7 +22,11 @@ class PonentesController {
         $total = Ponente::total();
         $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
 
-        $ponentes = Ponente::all();
+        if($paginacion->total_paginas() < $pagina_actual){
+            header('Location: /admin/ponentes?page=1');
+        }
+
+        $ponentes = Ponente::paginar($registros_por_pagina, $paginacion->offset());
 
         if(!is_admin()){
             header('Location: /login');

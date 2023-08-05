635b30b463575534f3507fc482f126a07621eb8e
Moises Arcos <acm.developer.19@gmail.com>
2023-08-05 05:45:03 -0600

    DWC-730: Crear Tabla para Días, Modelo y mostrar en el formulario

--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -35,6 +35,22 @@
             <?php } ?>
         </select>
     </div>
+    <div class="formulario__campo">
+        <label for="descripcion" class="formulario__label">Categoria o Tipo de Evento</label>
+        <div class="formulario__radio">
+            <?php foreach($dias as $dia){ ?>
+                <div>
+                    <label for="<?php echo strtolower($dia->nombre);?>"><?php echo $dia->nombre;?></label>
+                    <input
+                        type="radio"
+                        id="<?php echo strtolower($dia->nombre);?>"
+                        name="dia"
+                        value="<?php echo $dia->id;?>"
+                    />
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 
 
 </fieldset>

79e4c1ce6b7e7a149edce0bc4b8bb8a0afcd887c
Moises Arcos <acm.developer.19@gmail.com>
2023-08-30 05:46:21 -0600

    DWC-759: Ordenar los eventos en el backend

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -2,6 +2,11 @@
 
 namespace Controllers;
 
+use Model\Categoria;
+use Model\Dia;
+use Model\Evento;
+use Model\Hora;
+use Model\Ponente;
 use MVC\Router;
 
 class PaginasController{
@@ -28,6 +33,35 @@ class PaginasController{
     }
 
     public static function conferencias(Router $router){
+
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+
+        $eventos_formateados = [];
+        foreach($eventos as $evento){
+
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+
+            if($evento->dia_id === "1" && $evento->categoria_id === "1"){
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "1"){
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+
+            if($evento->dia_id === "1" && $evento->categoria_id === "2"){
+                $eventos_formateados['workshops_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "2"){
+                $eventos_formateados['workshops_s'][] = $evento;
+            }
+        }
+
+        debuguear($eventos_formateados);
 
         $router->render('paginas/conferencias', [
             'titulo' => 'Conferencias & Workshops'

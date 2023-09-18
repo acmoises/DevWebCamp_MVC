ae2930bd593100955ed08b40ee4157ec4e88277c
Moises Arcos <acm.developer.19@gmail.com>
2023-09-18 06:18:15 -0600

    DWC-774: crear una funcion para añadir las animaciones

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -6,19 +6,19 @@ include_once __DIR__ . '/conferencias.php';
 
 <section class="resumen">
     <div class="resumen__grid">
-        <div data-aos="fade-up" class="resumen__bloque">
+        <div <?php aos_animacion();?> class="resumen__bloque">
             <p class="resumen__texto resumen__texto--numero"><?php echo $ponentes_total; ?></p>
             <p class="resumen__texto">Speakers</p>
         </div>
-        <div data-aos="fade-down" class="resumen__bloque">
+        <div <?php aos_animacion();?> class="resumen__bloque">
             <p class="resumen__texto resumen__texto--numero"><?php echo $conferencias_total; ?></p>
             <p class="resumen__texto">Conferencias</p>
         </div>
-        <div data-aos="fade-right" class="resumen__bloque">
+        <div <?php aos_animacion();?> class="resumen__bloque">
             <p class="resumen__texto resumen__texto--numero"><?php echo $workshops_total; ?></p>
             <p class="resumen__texto">Workshops</p>
         </div>
-        <div data-aos="fade-left" class="resumen__bloque">
+        <div <?php aos_animacion();?> class="resumen__bloque">
             <p class="resumen__texto resumen__texto--numero">500</p>
             <p class="resumen__texto">Asistentes</p>
         </div>
@@ -31,7 +31,7 @@ include_once __DIR__ . '/conferencias.php';
 
     <div class="speakers__grid">
         <?php foreach($ponentes as $ponente){ ?>
-            <div class="speaker">
+            <div <?php aos_animacion(); ?> class="speaker">
                 <picture>
                     <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $ponente->imagen; ?>.webp" type="image/webp">
                     <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $ponente->imagen; ?>.png" type="image/png">
@@ -103,17 +103,17 @@ include_once __DIR__ . '/conferencias.php';
     <h2 class="boletos__heading">Boletos & Precios</h2>
     <p class="boletos__descripcion">Precios para DevWebCamp</p>
     <div class="boletos__grid">
-        <div class="boleto boleto--presencial">
+        <div <?php aos_animacion(); ?> class="boleto boleto--presencial">
             <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
             <p class="boleto__plan">Presencial</p>
             <p class="boleto__precio">$199</p>
         </div>
-        <div class="boleto boleto--virtual">
+        <div <?php aos_animacion(); ?> class="boleto boleto--virtual">
             <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
             <p class="boleto__plan">Virtual</p>
             <p class="boleto__precio">$49</p>
         </div>
-        <div class="boleto boleto--gratis">
+        <div <?php aos_animacion(); ?> class="boleto boleto--gratis">
             <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
             <p class="boleto__plan">Gratis</p>
             <p class="boleto__precio">- $0</p>

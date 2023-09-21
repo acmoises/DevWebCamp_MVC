9de2e4c1be726039f8c3037a40fcdd18f3244a3a
Moises Arcos <acm.developer.19@gmail.com>
2023-09-21 06:22:18 -0600

    DWC-782: Crear el boton para el registro presencial

--- views/registro/crear.php
+++ views/registro/crear.php
@@ -23,6 +23,11 @@
                 <li class="paquete__elemento">Comida y Bebida</li>
             </ul>
             <p class="paquete__precio">$199</p>
+            <div id="smart-button-container">
+                <div style="text-align: center;">
+                    <div id="paypal-button-container"></div>
+                </div>
+            </div>
         </div>
         <div <?php aos_animacion(); ?> class="paquete">
             <h3 class="paquete__nombre">Pase Virtual</h3>
@@ -36,3 +41,46 @@
         </div>
     </div>
 </main>
+
+<script src="https://www.paypal.com/sdk/js?client-id=AZ7P7bx5m9CnVY1oNGlE2nrTPOdBdnhpxFa4sa00rA3W6diUoe6bpb8WfP6QwezlmX5QpEBZ9lGBnN3K&enable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
+ 
+<script>
+    function initPayPalButton() {
+      paypal.Buttons({
+        style: {
+          shape: 'rect',
+          color: 'blue',
+          layout: 'vertical',
+          label: 'pay',
+        },
+ 
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{"description":"1","amount":{"currency_code":"USD","value":199}}]
+          });
+        },
+ 
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(orderData) {
+ 
+            // Full available details
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+ 
+            // Show a success message within this page, e.g.
+            const element = document.getElementById('paypal-button-container');
+            element.innerHTML = '';
+            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+ 
+            // Or go to another URL:  actions.redirect('thank_you.html');
+            
+          });
+        },
+ 
+        onError: function(err) {
+          console.log(err);
+        }
+      }).render('#paypal-button-container');
+    }
+ 
+  initPayPalButton();
+</script>

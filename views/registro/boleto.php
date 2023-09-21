cae053a95d85e130269b61734eeb1c5840114a71
Moises Arcos <acm.developer.19@gmail.com>
2023-09-21 05:00:25 -0600

    DWC-780: Finalizar el boleto virtual

--- views/registro/boleto.php
+++ views/registro/boleto.php
@@ -2,7 +2,7 @@
     <h2 class="pagina__heading"><?php echo $titulo; ?></h2>
     <p class="pagina__descripcion">Tu Boleto - Te recomendamos almacenarlo, tambien puedes compartirlo en redes sociales</p>
 
-    <div class="boleto boleto--<?php echo strtolower($registro->paquete->nombre); ?>">
+    <div class="boleto boleto--<?php echo strtolower($registro->paquete->nombre); ?> boleto--acceso">
         <div class="boleto__contenido">
             <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
             <p class="boleto__plan"><?php echo $registro->paquete->nombre; ?></p>
